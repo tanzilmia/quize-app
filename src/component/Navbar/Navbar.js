fa6f3594f91913340592ff5094b35dc6d90d5ced
tanzilmia <ziltan590@gmail.com>
2022-10-11 11:10:36 +0600

    navbar design

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+const [open, setopen] = useState(false)
+
     return (
-        <div>
-            <h2> working </h2>
-            <ul>
-                <NavLink to= '/'>Home</NavLink>
-                <NavLink to= '/statistics '>Statistics</NavLink>
-                <NavLink to= '/blog '>Blog </NavLink>
-            </ul>
+        <div className='shadow-md w-full fixed top-0 left-0'>
+            <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+              <div className='font-bold text-2xl cursor-pointer flex items-center text-gray-800'>
+                x - Accademy
+              </div>
+
+              <div onClick={()=> setopen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+              <ion-icon name= {open ? 'close' : 'menu'}></ion-icon>
+              {/* <ion-icon name="close"></ion-icon> */}
+              </div>
+
+              <ul className= {`md:flex md:items-center md:pb-0 pb-10 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20': 'top-[-500px]' }`}>
+
+                <NavLink className='md:ml-8 my-5 md:my-0 text-xl text-gray-800 hover:text-gray-400 duration-500  block' to= '/'>Home</NavLink>
+                <NavLink className='md:ml-8 my-5 md:my-0 text-xl text-gray-800 hover:text-gray-400 duration-500  block' to= '/statistics '>Statistics</NavLink>
+                <NavLink className='md:ml-8 my-5 md:my-0 text-xl text-gray-800 hover:text-gray-400 duration-500  block' to= '/blog '>Blog </NavLink>
+              </ul>
+            </div>
         </div>
     );
 };
