e1fa523e8079edada78cdbd4a9ca00c28d47a9ba
tanzilmia <ziltan590@gmail.com>
2022-10-11 16:31:29 +0600

    show correct answer

--- src/component/Quiz/Quiz.js
+++ src/component/Quiz/Quiz.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 const Quiz = ({questionbank}) => {
     let {correctAnswer,options,question,} = questionbank
+
+   const seeCorrectAns = () =>{
+     let correct = options.find(option=> option === correctAnswer)
+     if(correct){
+        alert(`correct answer is ${correct}`)
+     }else{
+        alert('not found')
+     }
+   }
+
     const submitAnswer = event => {
        let mysubmit = event.target.innerText
        if(mysubmit === correctAnswer){
@@ -12,10 +22,13 @@ const Quiz = ({questionbank}) => {
       }
     return (
         <div className = 'p-4 border w-full my-2 rounded-md shadow-sm'>
+            <div className='flex justify-between'>
             <h3 className='text-xl text-indigo-800'> {question} </h3>
-            <div className='p-2 flex flex-wrap'>
+            <button onClick={seeCorrectAns} className ='text-indigo-800 text-xl'> <ion-icon name="eye"></ion-icon> </button>
+            </div>
+            <div className='p-2'>
             {
-                options.map(option => <p className='cursor-pointer basis-1/2 border w-80 m-1 p-4' key = {option.toString()} onClick={submitAnswer}> {option} </p>)
+                options.map(option => <p className='cursor-pointer border md:w-3/5 m-1 p-4' key = {option.toString()} onClick={submitAnswer}> {option} </p>)
             }
             </div>
         </div>
