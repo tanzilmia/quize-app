17a5312b79b14f6deb4b7bd1f5ace337dfefb3cf
tanzilmia <ziltan590@gmail.com>
2022-10-12 11:06:52 +0600

    redmi file reset

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='header text-center'>
             <h1 className='text-white text-center mt-9 text-5xl font-semibold'> Improve Your Skill , For Get Better Job !!</h1>
-            
+             <p>Today Learn tomorrow Be a Good Leader</p>
             <button className='b bg-green-500 py-3 px-5 rounded-md text-center mt-8 text-lime-50'>Explore Naw</button>
             
         </div>
